f651932bc0f91e11bad47d18ccef678af3e12007
Illeyrocci <40275990+Illeyrocci@users.noreply.github.com>
2024-03-25 02:47:27 +0300

    loading state in Fragment added

--- app/src/main/java/com/bignerdranch/android/photogallery/ui/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/ui/PhotoGalleryFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.GridLayoutManager
 import com.bignerdranch.android.photogallery.Injection
 import com.bignerdranch.android.photogallery.databinding.FragmentPhotoGalleryBinding
@@ -45,7 +48,35 @@ class PhotoGalleryFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            retryButton.setOnClickListener { adapter.retry() }
+            viewLifecycleOwner.lifecycleScope.launch {
+                viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                    adapter.loadStateFlow.collect { loadState ->
+                        val isListEmpty =
+                            loadState.refresh is LoadState.NotLoading && adapter.itemCount == 0
+                        emptyList.isVisible = isListEmpty
+                        photoGrid.isVisible = !isListEmpty
+                        progressBar.isVisible = loadState.source.refresh is LoadState.Loading
+                        retryButton.isVisible = loadState.source.refresh is LoadState.Error
 
+                        // Toast on any error, regardless of whether it came from RemoteMediator or PagingSource
+                        val errorState = loadState.source.append as? LoadState.Error
+                            ?: loadState.source.prepend as? LoadState.Error
+                            ?: loadState.append as? LoadState.Error
+                            ?: loadState.prepend as? LoadState.Error
+                        errorState?.let {
+                            Toast.makeText(
+                                requireContext(),
+                                "\uD83D\uDE28 Wooops ${it.error}",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+
+                    }
+                }
+            }
+        }
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 photoGalleryViewModel.pagingDataFlow.collectLatest(adapter::submitData)
